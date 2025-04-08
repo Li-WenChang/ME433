e52447c8f0177892a3c1c89fe0a793a090c50252
Chang-Li-Wen <steven890919@gmail.com>
2025-04-07 20:18:08 -0500

    finish the hw2
    the folder called hello_usb is my hw2.

--- HW2/hello_gpio_irq/hello_gpio_irq.c
+++ HW2/hello_gpio_irq/hello_gpio_irq.c
@@ -10,52 +10,53 @@
 
 #define GPIO_WATCH_PIN 12
 
-volatile int led_status = -1;
+static char event_str[128];
 
-void pico_led_init(void) {
-    #ifdef PICO_DEFAULT_LED_PIN
-        // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
-        // so we can use normal GPIO functionality to turn the led on and off
-        gpio_init(PICO_DEFAULT_LED_PIN);
-        gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-    #endif
-    }
-    
-    // Turn the LED on or off
-    void pico_set_led(bool led_on) {
-    #if defined(PICO_DEFAULT_LED_PIN)
-        // Just set the GPIO on or off
-        gpio_put(PICO_DEFAULT_LED_PIN, led_on);
-    #endif
-    }
+void gpio_event_string(char *buf, uint32_t events);
 
-void gpio_callback(uint gpio, uint32_t events){
-
-    // if detect a falling edge and the digital input is low after 10ms, meaning that it's a real press
-    sleep_ms(10);
-    if (gpio_get(GPIO_WATCH_PIN) == false){
-
-        // toggle the led status
-        led_status *= -1;
-
-        if (led_status == 1){pico_set_led(true);}
-        else{pico_set_led(false);}
-    }
-
+void gpio_callback(uint gpio, uint32_t events) {
+    // Put the GPIO event(s) that just happened into event_str
+    // so we can print it
+    gpio_event_string(event_str, events);
+    printf("GPIO %d %s\n", gpio, event_str);
 }
 
 int main() {
     stdio_init_all();
-    pico_led_init();
 
     printf("Hello GPIO IRQ\n");
     gpio_init(GPIO_WATCH_PIN);
-
-    gpio_set_dir(GPIO_WATCH_PIN, GPIO_IN);
-
-    gpio_set_irq_enabled_with_callback(GPIO_WATCH_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(GPIO_WATCH_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 
     // Wait forever
-    while (1){}
+    while (1);
 }
 
+
+static const char *gpio_irq_str[] = {
+        "LEVEL_LOW",  // 0x1
+        "LEVEL_HIGH", // 0x2
+        "EDGE_FALL",  // 0x4
+        "EDGE_RISE"   // 0x8
+};
+
+void gpio_event_string(char *buf, uint32_t events) {
+    for (uint i = 0; i < 4; i++) {
+        uint mask = (1 << i);
+        if (events & mask) {
+            // Copy this event string into the user string
+            const char *event_str = gpio_irq_str[i];
+            while (*event_str != '\0') {
+                *buf++ = *event_str++;
+            }
+            events &= ~mask;
+
+            // If more events add ", "
+            if (events) {
+                *buf++ = ',';
+                *buf++ = ' ';
+            }
+        }
+    }
+    *buf++ = '\0';
+}
